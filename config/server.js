de758c749283a5c0f7b4bdef1f3260ca57d90bc1
Lautaro Nasello <lautaronasello@gmail.com>
2021-06-06 18:51:21 -0300

    strapimenoscaosback

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5418f6834e2a222e93c985b156ce6c74'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b94964ba-fd8b-41fb-9fd6-5bcfbcd072de'
+};
